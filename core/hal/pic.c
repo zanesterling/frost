7840352485da0d93b37183826e11b832b3b86d78
Zane Sterling <zsterling@google.com>
2016-07-25 14:50:46 -0700

    Add some abstraction fucntions to pic

--- core/hal/pic.c
+++ core/hal/pic.c
@@ -2,28 +2,51 @@
 
 int i86_pic_initialize() {
 	// ICW 1
-	outbyte(PIC_PORT_COMMAND,  0x11);
-	outbyte(PIC2_PORT_COMMAND, 0x11);
+	i86_pic_send_command(0x11, 0);
+	i86_pic_send_command(0x11, 1);
 
 	// ICW 2
-	outbyte(PIC_PORT_DATA_REG,  0x20);
-	outbyte(PIC2_PORT_DATA_REG, 0x28);
+	i86_pic_send_command(0x20, 0);
+	i86_pic_send_command(0x28, 1);
 
 	// ICW 3
-	outbyte(PIC_PORT_DATA_REG,  0x4);
-	outbyte(PIC2_PORT_DATA_REG, 0x2);
+	i86_pic_send_command(0x4, 0);
+	i86_pic_send_command(0x2, 1);
 
 	// ICW4
-	outbyte(PIC_PORT_DATA_REG,  0x1);
-	outbyte(PIC2_PORT_DATA_REG, 0x1);
-
-	// Null the data registers (unclear if necessary)
-	outbyte(PIC_PORT_DATA_REG,  0x0);
-	outbyte(PIC2_PORT_DATA_REG, 0x0);
+	i86_pic_send_command(0x1, 0);
+	i86_pic_send_command(0x1, 1);
 
 	return 0;
 }
 
-void sendeoi() {
+inline void sendeoi() {
 	outbyte(PIC_PORT_COMMAND, 0x20);
+}
+
+inline void i86_pic_send_command(uint8_t cmd, uint8_t pic) {
+	if (pic > 1) {
+		return;
+	}
+
+	uint8_t reg = pic ? I86_PIC2_PORT_COMMAND : I86_PIC_PORT_COMMAND;
+	outbyte(reg, cmd);
+}
+
+inline void i86_pic_send_data(uint8_t data, uint8_t pic) {
+	if (pic > 1) {
+		return;
+	}
+
+	uint8_t reg = pic ? I86_PIC2_PORT_DATA_REG : I86_PIC_PORT_DATA_REG;
+	outbyte(reg, data);
+}
+
+inline uint8_t i86_pic_read_data(uint8_t pic) {
+	if (pic > 1) {
+		return;
+	}
+
+	uint8_t reg = pic ? I86_PIC2_PORT_DATA_REG : I86_PIC_PORT_DATA_REG;
+	inbyte(reg);
 }
--- core/hal/pic.h
+++ core/hal/pic.h
@@ -4,14 +4,31 @@
 #include "idt.h"
 #include "stdint.h"
 
-#define PIC_PORT_COMMAND     0x20
-#define PIC_PORT_STATUS_REG  0x20
-#define PIC_PORT_MASK_REG    0x21
-#define PIC_PORT_DATA_REG    0x21
-#define PIC2_PORT_COMMAND    0xA0
-#define PIC2_PORT_STATUS_REG 0xA0
-#define PIC2_PORT_MASK_REG   0xA1
-#define PIC2_PORT_DATA_REG   0xA1
+// PIC ports
+#define I86_PIC_PORT_COMMAND     0x20
+#define I86_PIC_PORT_STATUS_REG  0x20
+#define I86_PIC_PORT_MASK_REG    0x21
+#define I86_PIC_PORT_DATA_REG    0x21
+#define I86_PIC2_PORT_COMMAND    0xA0
+#define I86_PIC2_PORT_STATUS_REG 0xA0
+#define I86_PIC2_PORT_MASK_REG   0xA1
+#define I86_PIC2_PORT_DATA_REG   0xA1
+
+// PIC1 devices
+#define I86_PIC_IRQ_TIMER      0
+#define I86_PIC_IRQ_KEYBOARD   1
+#define I86_PIC_IRQ_SERIAL2    3
+#define I86_PIC_IRQ_SERIAL1    4
+#define I86_PIC_IRQ_PARALLEL2  5
+#define I86_PIC_IRQ_DISKETTE   6
+#define I86_PIC_IRQ_PARALLEL1  7
+
+// PIC2 devices
+#define I86_PIC_IRQ_CMOSTIMER  0
+#define I86_PIC_IRQ_CGARETRACE 1
+#define I86_PIC_IRQ_AUXILIARY  4
+#define I86_PIC_IRQ_FPU        5
+#define I86_PIC_IRQ_HDC        6
 
 int i86_pic_initialize();
 void sendoi();
