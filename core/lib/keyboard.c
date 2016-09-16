f6d005dc9e20e507a50b50aa5f03c11c28daec73
Zane Sterling <sterling.zane@gmail.com>
2016-09-16 14:17:44 -0400

    Consolidate switch statements

--- core/lib/keyboard.c
+++ core/lib/keyboard.c
@@ -351,86 +351,81 @@ char kbrd_key_to_ascii(KEYCODE code) {
 		}
 
 		if (_shift && !_capslock) {
-			if (key >= '0' && key <= '9') {
-				switch (key) {
+			switch (key) {
 
-					case '0':
-						key = KEY_RIGHTPARENTHESIS;
-						break;
-					case '1':
-						key = KEY_EXCLAMATION;
-						break;
-					case '2':
-						key = KEY_AT;
-						break;
-					case '3':
-						key = KEY_EXCLAMATION;
-						break;
-					case '4':
-						key = KEY_HASH;
-						break;
-					case '5':
-						key = KEY_PERCENT;
-						break;
-					case '6':
-						key = KEY_CARRET;
-						break;
-					case '7':
-						key = KEY_AMPERSAND;
-						break;
-					case '8':
-						key = KEY_ASTERISK;
-						break;
-					case '9':
-						key = KEY_LEFTPARENTHESIS;
-						break;
-				}
-			} else {
-				switch (key) {
-					case KEY_COMMA:
-						key = KEY_LESS;
-						break;
+				case '0':
+					key = KEY_RIGHTPARENTHESIS;
+					break;
+				case '1':
+					key = KEY_EXCLAMATION;
+					break;
+				case '2':
+					key = KEY_AT;
+					break;
+				case '3':
+					key = KEY_EXCLAMATION;
+					break;
+				case '4':
+					key = KEY_HASH;
+					break;
+				case '5':
+					key = KEY_PERCENT;
+					break;
+				case '6':
+					key = KEY_CARRET;
+					break;
+				case '7':
+					key = KEY_AMPERSAND;
+					break;
+				case '8':
+					key = KEY_ASTERISK;
+					break;
+				case '9':
+					key = KEY_LEFTPARENTHESIS;
+					break;
+				case KEY_COMMA:
+					key = KEY_LESS;
+					break;
 
-					case KEY_DOT:
-						key = KEY_GREATER;
-						break;
+				case KEY_DOT:
+					key = KEY_GREATER;
+					break;
 
-					case KEY_SLASH:
-						key = KEY_QUESTION;
-						break;
+				case KEY_SLASH:
+					key = KEY_QUESTION;
+					break;
 
-					case KEY_SEMICOLON:
-						key = KEY_COLON;
-						break;
+				case KEY_SEMICOLON:
+					key = KEY_COLON;
+					break;
 
-					case KEY_QUOTE:
-						key = KEY_QUOTEDOUBLE;
-						break;
+				case KEY_QUOTE:
+					key = KEY_QUOTEDOUBLE;
+					break;
 
-					case KEY_LEFTBRACKET:
-						key = KEY_LEFTCURL;
-						break;
+				case KEY_LEFTBRACKET:
+					key = KEY_LEFTCURL;
+					break;
 
-					case KEY_RIGHTBRACKET:
-						key = KEY_RIGHTCURL;
-						break;
+				case KEY_RIGHTBRACKET:
+					key = KEY_RIGHTCURL;
+					break;
 
-					case KEY_GRAVE:
-						key = KEY_TILDE;
-						break;
+				case KEY_GRAVE:
+					key = KEY_TILDE;
+					break;
 
-					case KEY_MINUS:
-						key = KEY_UNDERSCORE;
-						break;
+				case KEY_MINUS:
+					key = KEY_UNDERSCORE;
+					break;
 
-					case KEY_PLUS:
-						key = KEY_EQUAL;
-						break;
+				case KEY_PLUS:
+					key = KEY_EQUAL;
+					break;
 
-					case KEY_BACKSLASH:
-						key = KEY_BAR;
-						break;
-				}
+				case KEY_BACKSLASH:
+					key = KEY_BAR;
+					break;
 			}
 		}
 
