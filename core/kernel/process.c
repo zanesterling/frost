0b9df05a92b0a77e1a1e669b9a0a26b6782ef8f3
Zane Sterling <sterling.zane@gmail.com>
2016-10-21 22:33:25 -0400

    Add process allocation

--- core/kernel/process.c
+++ core/kernel/process.c
@@ -1,7 +1,9 @@
 #include "process.h"
 
 #define MAX_PROCS 256
+SpinLock _proclist_lock;
 struct process* _proclist;
+int _num_procs;
 
 #define _is_valid_pid(pid) ((pid) > 0 && (pid) < MAX_PROCS)
 
@@ -11,7 +13,35 @@ int init_proclist() {
 	size_t mem_needed = MAX_PROCS * sizeof(struct process);
 	_proclist = (struct process*) pmem_alloc_memory(mem_needed);
 	if (_proclist == NULL) return -1;
+
+	for (int i = 0; i < MAX_PROCS; i++) {
+		_proclist[i].pid = i;
+		_proclist[i].in_use = false;
+	}
+
+	_num_procs = 0;
+	_proclist_lock.locked = 0;
 	return 0;
+}
+
+struct process* new_process() {
+	lock(&_proclist_lock);
+	if (_num_procs >= MAX_PROCS) {
+		unlock(&_proclist_lock);
+		return NULL;
+	}
+
+	for (int i = 0; i < MAX_PROCS; i++) {
+		if (_proclist[i].in_use) continue;
+
+		_num_procs++;
+		_proclist[i].in_use = false;
+		unlock(&_proclist_lock);
+		return _proclist + i;
+	}
+
+	unlock(&_proclist_lock);
+	return NULL;
 }
 
 struct process* get_process(uint32_t pid) {
--- core/kernel/process.h
+++ core/kernel/process.h
@@ -3,9 +3,13 @@
 #include <pmem.h>
 #include <stdint.h>
 
+#include "spinlock.h"
+
 struct process {
 	uint32_t pid;
+	bool in_use;
 };
 
 int init_proclist();
+struct process* new_process();
 struct process* get_process(uint32_t pid);
