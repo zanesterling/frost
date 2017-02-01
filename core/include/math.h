68bbc5b6e34c742305bec3c043114cc4f264fbac
Zane Sterling <sterling.zane@gmail.com>
2017-02-01 16:25:03 -0500

    Implement arc functions, sqrt

--- core/include/math.h
+++ core/include/math.h
@@ -4,6 +4,14 @@
 
 #define M_PI 3.14159265358979323
 #define M_TAU (M_PI * 2)
+#define SQRT_3 1.73205081
+
+#define abs(x) (x < 0 ? -x : x)
 
 float sin(float theta);
 float cos(float theta);
+float asin(float theta);
+float acos(float theta);
+float atan(float theta);
+
+float sqrt(float x);
--- core/lib/math.c
+++ core/lib/math.c
@@ -58,3 +58,55 @@ float cos(float theta) {
 	#define theta_6 (theta_4 * theta_2)
 	return factor * (1 - theta_2 / 2 + theta_4 / 24 - theta_6 / 720);
 }
+
+float asin(float x) {
+	return atan(x / sqrt(1 - x * x));
+}
+
+float acos(float x) {
+	return atan(sqrt(1 - x * x) / x);
+}
+
+float atan(float x) {
+	// Restrict to positive values.
+	float factor_1 = 1;
+	float offset_1 = 0;
+	if (x < 0) {
+		x = -x;
+		factor_1 = -factor_1;
+	}
+
+	// Restrict to [0, 1].
+	float factor_2 = 1;
+	float offset_2 = 0;
+	if (x > 1) {
+		factor_2 = -1;
+		offset_2 = M_PI / 2;
+		x = 1 / x;
+	}
+
+	// Restrict to [0, 2 - sqrt(3)]
+	float factor_3 = 1;
+	float offset_3 = 0;
+	if (x > 0.268) {
+		offset_3 = M_PI / 6;
+		x = (SQRT_3 * x - 1) / (SQRT_3 + x);
+	}
+
+	// Use the polynomial
+	float result_3 = x - x*x*x / 3 + x*x*x*x*x / 5;
+	float result_2 = result_3 * factor_3 + offset_3;
+	float result_1 = result_2 * factor_2 + offset_2;
+	return result_1 * factor_1 + offset_1;
+}
+
+float sqrt(float x) {
+	float guess = x / 2;
+	float epsilon = 0.00001;
+
+	while (abs(guess * guess - x) > epsilon) {
+		guess = guess / 2 + x / (2 * guess);
+	}
+
+	return guess;
+}
